a1aafe2f59c2d985356a78ebb0890fb7a90c27a9
Diogo-Cardeal <didiogomc15@gmail.com>
2026-03-15 11:37:08 -0700

    first commit

---
+++ Teste/script.js
@@ -0,0 +1,4 @@
+document.getElementById('showPhraseButton').addEventListener('click', function() {
+    const phraseElement = document.getElementById('phrase');
+    phraseElement.classList.toggle('hidden');
+});
